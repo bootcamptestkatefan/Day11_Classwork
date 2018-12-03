ae92ce33d813c2653f23a413f01454efed96cf2e
bootcamptestkatefan <kate.fan@oocl.com>
2018-12-03 16:58:59 +0800

    [kate]done task 7

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Number from './components/Number'
 
 class App extends Component {
   state= {number: 0}
   updateNumber = () => {
-    alert("now you know how to handle onclick event");
-    this.setState({number: 1}); 
+    this.setState({number: this.props.onClickWho(this.state.number)}); 
   }
   render() {
     return (
       <div>
-        <button onClick={this.updateNumber}>click me</button>
-        <span>number: {this.state.number}</span>
+        <button onClick={this.updateNumber}>Add 2</button>
+        <span><Number /> {this.state.number}</span>
 
       </div>
     );
